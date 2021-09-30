f296f7623bd6edcc8b0c2990184bb4a27f80115c
Thiago Dias <thiagolutzdias@hotmail.com>
2021-09-30 14:39:13 -0400

    Atualizado estado do aplicativo admin

---
+++ admin/lib/shared/themes/app_colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const purpleDark = Color(0xFF8F4599);
+  static const purpleMedium = Color(0xFFA468AC);
+  static const purpleLight = Color(0xFFBC91C2);
+  static const orangeDark = Color(0xFFFF9B0D);
+  static const orangeMedium = Color(0xFFFFB54B);
+  static const orangeLight = Color(0xFFFFCB82);
+  static const whiteOrange = Color(0xFFF9EBD7);
+  static const grey = Color(0xFFF2EDE5);
+  static const whiteDark = Color(0xFFF6F4F1);
+  static const white = Color(0xFFFAFAFA);
+  static const text = Color(0xFF434343);
+}
---
+++ admin/lib/shared/themes/app_text_styles.dart
@@ -0,0 +1,142 @@
+import 'package:admin/shared/themes/app_colors.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+// Proprietários
+
+class AppTextStyles {
+  static final buttons = GoogleFonts.poppins(
+    fontSize: 20,
+    fontWeight: FontWeight.w700,
+    color: const Color(0xFFFFFFFF),
+  );
+  static final homeButtonOrange = GoogleFonts.roboto(
+    fontSize: 20,
+    fontWeight: FontWeight.w500,
+    color: AppColors.orangeDark,
+  );
+  static final homeBarSelected = GoogleFonts.roboto(
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    color: const Color(0xFFFFFFFF),
+  );
+  static final homeBarOption = GoogleFonts.roboto(
+    // Opacidade 80%
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    color: const Color(0xFFFFFFFF),
+  );
+  static final homeNameBlack = GoogleFonts.roboto(
+    fontSize: 24,
+    fontWeight: FontWeight.w900,
+    color: const Color(0xFF333333),
+  );
+  static final homeNameOrange = GoogleFonts.roboto(
+    fontSize: 24,
+    fontWeight: FontWeight.w900,
+    color: AppColors.orangeDark,
+  );
+  static final homeSaleBlack = GoogleFonts.roboto(
+    fontSize: 22,
+    fontWeight: FontWeight.w900,
+    color: AppColors.text,
+  );
+  static final homeSaleOrange = GoogleFonts.roboto(
+    fontSize: 22,
+    fontWeight: FontWeight.w900,
+    color: AppColors.orangeDark,
+  );
+  static final itemName = GoogleFonts.roboto(
+    fontSize: 12,
+    fontWeight: FontWeight.w700,
+    color: AppColors.text,
+  );
+  static final itemPrice = GoogleFonts.roboto(
+    fontSize: 20,
+    fontWeight: FontWeight.w700,
+    color: AppColors.text,
+  );
+  static final itemPriceSale = GoogleFonts.roboto(
+    // Opacidade - 80%
+    fontSize: 15,
+    fontWeight: FontWeight.w700,
+    color: AppColors.text,
+  );
+  static final itemCifrao = GoogleFonts.roboto(
+    fontSize: 10,
+    fontWeight: FontWeight.w700,
+    color: AppColors.orangeDark,
+  );
+  static final itemCifraoHome = GoogleFonts.roboto(
+    fontSize: 10,
+    fontWeight: FontWeight.w700,
+    color: AppColors.text,
+  );
+  static final itemCifraoSale = GoogleFonts.roboto(
+    // Opacidade - 80%
+    fontSize: 8,
+    fontWeight: FontWeight.w700,
+    color: AppColors.text,
+  );
+  static final menuOption = GoogleFonts.roboto(
+    fontSize: 18,
+    fontWeight: FontWeight.w700,
+    color: const Color(0xFFFFFFFF),
+  );
+  static final menuSelected = GoogleFonts.roboto(
+    fontSize: 24,
+    fontWeight: FontWeight.w900,
+    color: AppColors.orangeDark,
+  );
+  static final appBar = GoogleFonts.roboto(
+    fontSize: 24,
+    fontWeight: FontWeight.w900,
+    color: const Color(0xFFFFFFFF),
+  );
+  static final contactTitle = GoogleFonts.roboto(
+    fontSize: 18,
+    fontWeight: FontWeight.w900,
+    color: AppColors.orangeMedium,
+  );
+  static final contactInfo = GoogleFonts.roboto(
+    fontSize: 15,
+    fontWeight: FontWeight.w500,
+    color: AppColors.orangeMedium,
+  );
+  static final contactText = GoogleFonts.roboto(
+    fontSize: 13,
+    fontWeight: FontWeight.w500,
+    color: AppColors.orangeMedium,
+  );
+  static final contactDetail = GoogleFonts.roboto(
+    fontSize: 10,
+    fontWeight: FontWeight.w500,
+    color: AppColors.orangeMedium,
+  );
+  static final infoTitle = GoogleFonts.roboto(
+    fontSize: 24,
+    fontWeight: FontWeight.w700,
+    color: AppColors.text,
+  );
+  static final infoSection = GoogleFonts.roboto(
+    fontSize: 18,
+    fontWeight: FontWeight.w900,
+    color: AppColors.text,
+  );
+  static final infoText = GoogleFonts.roboto(
+    // Opacidade - 60%
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    color: AppColors.text,
+  );
+  static final infoPrice = GoogleFonts.roboto(
+    fontSize: 30,
+    fontWeight: FontWeight.w700,
+    color: AppColors.text,
+  );
+  static final infoCifrao = GoogleFonts.roboto(
+    fontSize: 16,
+    fontWeight: FontWeight.w700,
+    color: AppColors.orangeDark,
+  );
+}
