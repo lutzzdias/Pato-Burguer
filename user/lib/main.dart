38f1c4c2c5eb2a242821305a823a2b1f12550c2d
Thiago Dias <thiagolutzdias@hotmail.com>
2021-09-30 15:35:58 -0400

    Criada tela home com identificação apenas

--- user/lib/main.dart
+++ user/lib/main.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:pato_burguer/modules/inical/inicial_page.dart';
+import 'package:pato_burguer/screens/home/home.dart';
 import 'package:pato_burguer/shared/themes/app_colors.dart';
 
-void main(){
+void main() {
   runApp(AppWidget());
 }
 
-class AppWidget extends StatelessWidget{
-  @override 
+class AppWidget extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Pato Burguer',
       theme: ThemeData(primaryColor: AppColors.white),
-      home: InicialPage(),
+      home: const Home(),
     );
   }
 }
