fdfb298b055c4ff36c9d8ca2c1047b327e80413f
Thiago Dias <thiagolutzdias@hotmail.com>
2021-10-21 13:57:44 -0300

    Adicionada tela de informações ao clicar no cardápio já implementada com firebase

--- user/lib/screens/info/info.dart
+++ user/lib/screens/info/info.dart
@@ -1,13 +1,149 @@
 import 'package:flutter/material.dart';
+import 'package:pato_burguer/screens/menu/item.dart';
+import 'package:pato_burguer/shared/themes/app_colors.dart';
+import 'package:pato_burguer/shared/themes/app_images.dart';
+import 'package:pato_burguer/shared/themes/app_text_styles.dart';
 
 class Info extends StatelessWidget {
-  const Info({Key? key}) : super(key: key);
+  final Item? item;
+
+  const Info({Key? key, this.item}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColors.orangeDark,
       appBar: AppBar(
-        title: const Text("Info"),
+        title: Padding(
+          padding: const EdgeInsets.only(left: 60),
+          child: Text(
+            'Cardápio',
+            style: AppTextStyles.appBar,
+          ),
+        ),
+        backgroundColor: AppColors.orangeDark,
+        shadowColor: AppColors.orangeDark,
+        foregroundColor: AppColors.orangeDark,
+        elevation: 0,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Padding(
+            padding: const EdgeInsets.only(left: 10),
+            child: const Icon(Icons.arrow_back_ios),
+          ),
+          color: AppColors.white,
+          iconSize: 30,
+        ),
+      ),
+      body: Stack(
+        children: [
+          SizedBox(
+            height: 60,
+          ),
+          Container(
+            padding: EdgeInsets.only(left: 0.8, right: 0.8, top: 135),
+            height: MediaQuery.of(context).size.height * 1.0,
+            width: double.infinity,
+            color: Colors.transparent,
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(15),
+                    topRight: Radius.circular(15),
+                  )),
+              child: Column(
+                children: [
+                  Container(
+                      child: Row(
+                    children: [
+                      Padding(
+                        padding:
+                            const EdgeInsets.only(top: 70, left: 27, right: 0),
+                        child: Text(
+                          'Nome',
+                          style: TextStyle(
+                              fontSize: 18,
+                              fontFamily: 'roboto',
+                              fontWeight: FontWeight.bold),
+                          textAlign: TextAlign.left,
+                        ),
+                      ),
+                    ],
+                  )),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 5, left: 22, right: 22),
+                    child: Container(
+                      height: 36,
+                      width: 313,
+                      child: Text(item!.nome),
+                    ),
+                  ),
+                  Container(
+                      child: Row(
+                    children: [
+                      Padding(
+                        padding:
+                            const EdgeInsets.only(top: 3, left: 27, right: 0),
+                        child: Text(
+                          'Detalhes',
+                          style: TextStyle(
+                              fontSize: 18,
+                              fontFamily: 'roboto',
+                              fontWeight: FontWeight.bold),
+                          textAlign: TextAlign.left,
+                        ),
+                      ),
+                    ],
+                  )),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 5, left: 22, right: 22),
+                    child: Container(
+                      height: 60,
+                      width: 313,
+                      child: Text(item!.desc),
+                    ),
+                  ),
+                  Container(
+                      child: Row(
+                    children: [
+                      Padding(
+                        padding:
+                            const EdgeInsets.only(top: 3, left: 27, right: 0),
+                        child: Text(
+                          'Preço',
+                          style: TextStyle(
+                              fontSize: 18,
+                              fontFamily: 'roboto',
+                              fontWeight: FontWeight.bold),
+                          textAlign: TextAlign.left,
+                        ),
+                      ),
+                    ],
+                  )),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 5, left: 22, right: 22),
+                    child: Container(
+                      height: 36,
+                      width: 313,
+                      child: Text("R\$ " + item!.preco.toString()),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            alignment: Alignment.topCenter,
+            height: MediaQuery.of(context).size.height / 3,
+            child: Image(
+              image: AssetImage('assets/images/' + item!.nomeImagem + '.png'),
+              fit: BoxFit.fitHeight,
+            ),
+          ),
+        ],
       ),
     );
   }
--- user/lib/screens/menu/item_design.dart
+++ user/lib/screens/menu/item_design.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:pato_burguer/screens/info/info.dart';
 import 'package:pato_burguer/screens/menu/item.dart';
 import 'package:pato_burguer/shared/themes/app_colors.dart';
 import 'package:pato_burguer/shared/themes/app_text_styles.dart';
@@ -11,81 +12,87 @@ class ItemDesign extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.transparent,
-        borderRadius: BorderRadius.circular(15),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withOpacity(.8),
-            blurRadius: 2,
-            spreadRadius: 0.25,
-            offset: const Offset(0, 1.5),
-          ),
-        ],
-      ),
-      child: Stack(
-        children: [
-          Column(
-            children: [
-              SizedBox(
-                width: double.infinity,
-                height: 60,
-                child: Container(
-                  decoration: const BoxDecoration(
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(15),
-                        topRight: Radius.circular(15)),
-                    color: AppColors.orangeLight,
+    return InkWell(
+      onTap: () {
+        Navigator.push(context,
+            MaterialPageRoute(builder: (context) => Info(item: this.item)));
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.transparent,
+          borderRadius: BorderRadius.circular(15),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(.8),
+              blurRadius: 2,
+              spreadRadius: 0.25,
+              offset: const Offset(0, 1.5),
+            ),
+          ],
+        ),
+        child: Stack(
+          children: [
+            Column(
+              children: [
+                SizedBox(
+                  width: double.infinity,
+                  height: 60,
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(15),
+                          topRight: Radius.circular(15)),
+                      color: AppColors.orangeLight,
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(
-                width: double.infinity,
-                height: 95,
-                child: Container(
-                  decoration: const BoxDecoration(
-                    borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(15),
-                        bottomRight: Radius.circular(15)),
-                    color: AppColors.fullWhite,
+                SizedBox(
+                  width: double.infinity,
+                  height: 95,
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(15),
+                          bottomRight: Radius.circular(15)),
+                      color: AppColors.fullWhite,
+                    ),
                   ),
                 ),
-              ),
-            ],
-          ),
-          Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 10),
-                child: SizedBox(
-                  height: 90,
-                  width: 150,
-                  child: Image.asset(
-                    ('assets/images/' + item.nomeImagem + '.png'),
-                    alignment: Alignment.topCenter,
-                    fit: BoxFit.fill,
+              ],
+            ),
+            Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 10),
+                  child: SizedBox(
+                    height: 90,
+                    width: 150,
+                    child: Image.asset(
+                      ('assets/images/' + item.nomeImagem + '.png'),
+                      alignment: Alignment.topCenter,
+                      fit: BoxFit.fitHeight,
+                    ),
                   ),
                 ),
-              ),
-              Text(
-                item.nome,
-                style: AppTextStyles.itemName,
-              ),
-              RichText(
-                text: TextSpan(
-                    text: "R\$ ",
-                    style: AppTextStyles.itemCifrao,
-                    children: [
-                      TextSpan(
-                        text: item.preco.toString(),
-                        style: AppTextStyles.itemPrice,
-                      ),
-                    ]),
-              ),
-            ],
-          ),
-        ],
+                Text(
+                  item.nome,
+                  style: AppTextStyles.itemName,
+                ),
+                RichText(
+                  text: TextSpan(
+                      text: "R\$ ",
+                      style: AppTextStyles.itemCifrao,
+                      children: [
+                        TextSpan(
+                          text: item.preco.toString(),
+                          style: AppTextStyles.itemPrice,
+                        ),
+                      ]),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
